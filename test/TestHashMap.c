9d9fcb9fb7e1f17e93ba6c9f76340f64c4c343ac
Matt McFarland <matthew.r.mcfarland.16@dartmouth.edu>
2015-10-31 02:13:27 -0400

    enough for tonight

--- test/TestHashMap.c
+++ test/TestHashMap.c
@@ -2,18 +2,45 @@
 // KPCB project test file
 
 #include <stdio.h>
+#include <limits.h>
+#include <string.h>
 #include "../src/hashmap.h"
+
+#define SIZE 50
 
 int main(void) {
 
 	printf("This is the test program for Matt McFarland's HashMap.\n");
+	int ret_val = 0;
+	//void * ret_ptr;
 
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++){
+		arr[i] = i;
+	}
 
-	// create hashmap
-	// size 0
-	// size negative
-	// size max_int
+	/* --- create hashmap tests --- */
+
 	// regular size
+	HashMap * map0 = CreateMap(SIZE);
+	if (!map0)
+		ret_val++;
+
+	// size 0
+	HashMap * map1 = CreateMap(0);
+	if (map1 != NULL)
+		ret_val++;
+
+	// size negative
+	HashMap * map2 = CreateMap(-6);
+	if (map2 != NULL)
+		ret_val++;
+
+	// size max_int
+	HashMap * map3 = CreateMap(INT_MAX);
+	if (map3 != NULL)
+		ret_val++;
+	
 	
 	// testing addition of values
 	// add a null key
@@ -22,7 +49,8 @@ int main(void) {
 	// change a previously set value
 	// what happens when you try to add to full map?
 	
-	
+
+	printf("ret_val = %d\n",ret_val);	
 
 	return 0;
 }
